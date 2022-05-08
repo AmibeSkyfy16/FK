ff8ce79ab79a8136b6b73a75c32f792b4ea4b936
AmibeSkyfy16 <colin.viatte@gmail.com>
2022-05-08 19:47:06 +0200

    tried something, but not work

--- src/main/java/ch/skyfy/fk/logic/FKGame.java
+++ src/main/java/ch/skyfy/fk/logic/FKGame.java
@@ -30,6 +30,7 @@ import net.minecraft.item.BucketItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.MessageType;
+import net.minecraft.scoreboard.AbstractTeam;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
@@ -44,6 +45,8 @@ import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static net.minecraft.util.Util.NIL_UUID;
 
@@ -62,9 +65,12 @@ public class FKGame {
         }
     }
 
+    public static AtomicBoolean configLoaded;
+
     static {
         ReflectionUtils.loadClassesByReflection(new Class[]{FKGameAllData.class});
         FKMod.LOGGER.info(FKGameAllData.class.getCanonicalName() + " loaded successfully");
+        configLoaded = new AtomicBoolean(false); // set to true in the end of the constructor
     }
 
     private final MinecraftServer server;
@@ -91,6 +97,8 @@ public class FKGame {
 
         initialize();
         registerEvents();
+
+        configLoaded.set(true);
     }
 
     private void initialize() {
@@ -151,11 +159,15 @@ public class FKGame {
             team = serverScoreboard.addTeam(GameUtils.getFKTeamIdentifierByName(fkTeam.getName())); // minecraft internal team name can't have space or special char
             team.setDisplayName(new LiteralText(fkTeam.getName()).setStyle(Style.EMPTY.withColor(Formatting.byName(fkTeam.getColor()))));
             team.setColor(Formatting.byName(fkTeam.getColor()));
+//            team.setPrefix(new LiteralText("["+team.getColor().getName().replace("_", "")+"]").setStyle(Style.EMPTY.withColor(team.getColor())));
+            team.setNameTagVisibilityRule(AbstractTeam.VisibilityRule.ALWAYS);
         }
+
 
         var playerTeam = serverScoreboard.getPlayerTeam(playerName);
         if (playerTeam == null)
             serverScoreboard.addPlayerToTeam(playerName, team);
+
 
         serverScoreboard.updateScoreboardTeamAndPlayers(team);
         serverScoreboard.updateScoreboardTeam(team);
--- src/main/java/ch/skyfy/fk/mixin/AbstractTeamMixin.java
+++ src/main/java/ch/skyfy/fk/mixin/AbstractTeamMixin.java
@@ -3,8 +3,9 @@ package ch.skyfy.fk.mixin;
 import ch.skyfy.fk.config.Configs;
 import ch.skyfy.fk.config.data.FKTeam;
 import ch.skyfy.fk.logic.FKGame;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -13,31 +14,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Objects;
+@Mixin(Team.class)
+public class AbstractTeamMixin {
 
-@Mixin(ServerPlayerEntity.class)
-public class ServerPlayerEntityMixin {
-
-    @Inject(method = "getPlayerListName", at = @At("TAIL"), cancellable = true)
-    private void replacePlayerListName(CallbackInfoReturnable<Text> cir) {
-
-        var player = (ServerPlayerEntity)(Object)this;
-
+    @Inject(at = @At("TAIL"), method = "decorateName(Lnet/minecraft/text/Text;)Lnet/minecraft/text/MutableText;", cancellable = true)
+    public void decorate(Text text, CallbackInfoReturnable<MutableText> callbackInfoReturnable) {
+        if(0 == 0)return;
         if(FKGame.configLoaded.get()) {
-            System.out.println("called after config !");
             for (FKTeam team : Configs.TEAMS.data.getTeams()) {
                 for (String playerName : team.getPlayers()) {
                     if(playerName.equals("Skyfy16") || playerName.equals("Alex")){
                         var mutableText = new LiteralText("[ "+team.getColor() + " ] " + playerName).setStyle(Style.EMPTY.withColor(Formatting.valueOf(team.getColor())));
-                        System.out.println("new text is : " + mutableText.getString());
-                        cir.setReturnValue(mutableText);
+                        text = mutableText;
+                        System.out.println("new text is : " + text.getString());
+                        callbackInfoReturnable.setReturnValue(mutableText);
                     }
                 }
             }
-        }else{
-            System.out.println("shit");
         }
-
     }
 
 }
